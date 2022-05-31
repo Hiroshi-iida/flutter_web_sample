2058f47ed795080a4b3a6b7d6cdbdb0054e611b4
Hiroshi-iida <iichan.hiro@gmail.com>
2022-05-31 15:22:16 +0900

    .envが読み込めてないっぽい

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 
 Future main() async {
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   // print(a);
   // LiffInitParam(liffId: '1656967377-6m2kVOQ7');
   // Liff.login(LiffLoginParam('https://liff.line.me/1656967377-6m2kVOQ7'));
@@ -23,8 +23,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   update() async {
     Uri uri = kReleaseMode
-        ? Uri.parse(dotenv.env['PROD_API_URL']!)
-        : Uri.parse(dotenv.env['DEV_API_URL']!);
+        ? Uri.parse('https://9m7i5r48jk.execute-api.us-east-1.amazonaws.com')
+        : Uri.parse('https://5mzw2ko471.execute-api.us-east-1.amazonaws.com');
     var result = await http.post(uri);
     setState(() {
       counter = int.parse(result.body);
