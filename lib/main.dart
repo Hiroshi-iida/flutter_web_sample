30c79684f12604d92c56e66a5401b927ff0e2190
Hiroshi-iida <iichan.hiro@gmail.com>
2022-05-31 20:28:14 +0900

    e n v

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   update() async {
     Uri uri = !kReleaseMode
-        ? Uri.parse(dotenv.env['PROD_API_URL']!)
-        : Uri.parse(dotenv.env['DEV_API_URL']!);
+        ? Uri.parse('https://5mzw2ko471.execute-api.us-east-1.amazonaws.com')
+        : Uri.parse('https://5mzw2ko471.execute-api.us-east-1.amazonaws.com');
     var result = await http.post(uri);
     setState(() {
       counter = int.parse(result.body);
