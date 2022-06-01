32b61ddc32fb318663075124e2a0f2c9a6de074c
Hiroshi-iida <iichan.hiro@gmail.com>
2022-06-01 13:56:39 +0900

    bottom navi 追加

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,25 +1,87 @@
 import 'package:flutter/material.dart';
-import 'dart:js' as js;
 
-import 'package:flutter_web_sample/example.dart';
+class Sample extends StatefulWidget {
+  const Sample({Key? key}) : super(key: key);
 
-class Sample extends StatelessWidget {
+  @override
+  State<Sample> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<Sample> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'sample material app',
-      home: Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('BottomNavigationBar Sample'),
+      // ),
+      body: Center(
+        child: Text(
+          "indexNum: $_selectedIndex",
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            activeIcon: Icon(Icons.book_online),
+            label: 'Book',
+            tooltip: "This is a Book Page",
+            backgroundColor: Colors.blue,
           ),
-          body: Center(
-            child: Text('next page'),
-          )),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            activeIcon: Icon(Icons.business_center),
+            label: 'Business',
+            tooltip: "This is a Business Page",
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            activeIcon: Icon(Icons.school_outlined),
+            label: 'School',
+            tooltip: "This is a School Page",
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings_accessibility),
+            label: 'Settings',
+            tooltip: "This is a Settings Page",
+            backgroundColor: Colors.pink,
+          ),
+        ],
+
+        type: BottomNavigationBarType.shifting,
+        // ここで色を設定していても、shiftingにしているので
+        // Itemの方のbackgroundColorが勝ちます。
+        backgroundColor: Colors.red,
+        enableFeedback: true,
+        // IconTheme系統の値が優先されます。
+        iconSize: 18,
+        // 横向きレイアウトは省略します。
+        // landscapeLayout: 省略
+        selectedFontSize: 20,
+        selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
+        selectedLabelStyle: const TextStyle(color: Colors.red),
+        // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
+        selectedItemColor: Colors.black,
+        unselectedFontSize: 15,
+        unselectedIconTheme: const IconThemeData(size: 25, color: Colors.white),
+        unselectedLabelStyle: const TextStyle(color: Colors.purple),
+        // IconTheme系統の値が優先されるのでこの値は適応されません。
+        unselectedItemColor: Colors.red,
+      ),
     );
   }
 }
